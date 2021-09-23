4c65f3ea26bd0d6f96588c25959d1530afbd1f58
LucasYFL <90341110+LucasYFL@users.noreply.github.com>
2021-09-23 19:13:52 -0400

    Update perf_test_all.sh

--- census/perf_test_all.sh
+++ census/perf_test_all.sh
@@ -1,4 +1,4 @@
 for i in 0.0 0.1 0.2 0.3 0.4 0.6 0.7 0.8 0.9 1.0
 do
-python perf_tests.py --filter sex --ratio_2 0.5 --ratio_2 $i
+python perf_tests.py --filter both --ratio_2 0.5 --ratio_2 $i
 done
