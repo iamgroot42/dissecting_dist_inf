d40efe38007384c3be2d1384d2b83283e73ef1cc
LucasYFL <90341110+LucasYFL@users.noreply.github.com>
2021-09-24 13:20:22 -0400

    Update do_all.sh

--- census/do_all.sh
+++ census/do_all.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
 for i in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
 do 
-./train_fast.sh adv both $i
-./train_fast.sh victim both $i
+./train_fast.sh adv $1 $i
+./train_fast.sh victim $1 $i
 done
 
-./perf_test_all.sh
+./perf_test_all.sh $1
+
+python meta.py --filter $1
