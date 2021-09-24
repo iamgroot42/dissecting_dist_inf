72cbf1d7b72b614eb14b78b7e5d6e5017c9633ac
LucasYFL <90341110+LucasYFL@users.noreply.github.com>
2021-09-23 21:21:12 -0400

    Update data_utils.py

--- census/data_utils.py
+++ census/data_utils.py
@@ -164,7 +164,10 @@ def get_filter(df, filter_prop, split, ratio, is_test, custom_limit=None,t_c=[1,
     elif filter_prop == "race":
         def lambda_fn(x): return x['race:White'] == 1
     elif filter_prop == "both":
-        def lambda_fn(x): return x['sex:Female'] == t_c[0] & x['race:White'] == t_c[1]
+        def lambda_fn(x): 
+            a = x['sex:Female'] == t_c[0] 
+            b = x['race:White'] == t_c[1]
+            return a & b
     # Rerun with 0.5:0.5
     prop_wise_subsample_sizes = {
         "adv": {
