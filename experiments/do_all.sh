ebd1cd294af78e6be5357d4f232dff0336164eed
LucasLYF <pdz6an@virginia.edu>
2022-05-26 10:51:26 -0400

    label noise

--- experiments/do_all.sh
+++ experiments/do_all.sh
@@ -6,7 +6,8 @@
 j=0
 for i in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
 do
-python train_more.py --load_config "./train_models.json" --split adv --ratio $i --gpu $j &
+python train_more.py --load_config "./train_nc_noise.json" --split victim --ratio $i --gpu $j &
+python train_more.py --load_config "./train_nc_noise.json" --split adv --ratio $i --gpu $j &
 j=$((($j+1)%4))
 if [ $j -ge 3 ]
 then
