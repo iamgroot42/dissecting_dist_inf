6b833bd6868d3303e396b131d0e96dce39d32b7c
LucasLYF <pdz6an@virginia.edu>
2022-06-20 15:36:31 -0400

    minor fix

--- rebase/distribution_inference/attacks/whitebox/comparison/comparison.py
+++ rebase/distribution_inference/attacks/whitebox/comparison/comparison.py
@@ -66,10 +66,8 @@ class ComparisonAttack:
         p1, p2 = sigmoid(preds_adv.preds_property_1), sigmoid(preds_adv.preds_property_2)
         # Predictions made by the  victim's models
         pv1, pv2 = sigmoid(preds_victim.preds_property_1), sigmoid(preds_victim.preds_property_2)
-        KL1 = (np.array([np.average([KL_func(p1_,pv1_) for p1_ in p1]) for pv1_ in pv1]),
-        np.array([np.average([KL_func(p2_,pv1_) for p2_ in p2]) for pv1_ in pv1]))
-        KL2 =  (np.array([np.average([KL_func(p1_,pv2_) for p1_ in p1]) for pv2_ in pv2]),
-        np.array([np.average([KL_func(p2_,pv2_) for p2_ in p2]) for pv2_ in pv2]))
+        KL1 = (np.array([KL_func(p1_,pv1_) for p1_,pv1_ in zip(p1,pv1)]),np.array([KL_func(p2_,pv1_) for p2_,pv1_ in zip(p2,pv1)]))
+        KL2 =  (np.array([KL_func(p1_,pv2_) for p1_,pv2_ in zip(p1,pv2)]),np.array([KL_func(p2_,pv2_) for p2_,pv2_ in zip(p2,pv2)]))
         res1 = KL1[1] - KL1[0]
         res2 = KL2[0] - KL2[1]
         acc1 = np.average(res1>=0)
@@ -79,8 +77,8 @@ class ComparisonAttack:
     def attack(self,
                preds_adv: PredictionsOnDistributions,
                preds_vic: PredictionsOnDistributions):
-        acc_1,preds_1 = self._attack_per_dis(preds_adv.preds_on_distr_1,preds_vic.preds_on_distr_1,self.config,epochwise_version)
-        acc_2,preds_2 = self._attack_per_dis(preds_adv.preds_on_distr_2,preds_vic.preds_on_distr_2,self.config,epochwise_version)
+        acc_1,preds_1 = self._attack_per_dis(preds_adv.preds_on_distr_1,preds_vic.preds_on_distr_1)
+        acc_2,preds_2 = self._attack_per_dis(preds_adv.preds_on_distr_2,preds_vic.preds_on_distr_2)
         # Get best adv accuracies for both distributions, across all ratios
         chosen_distribution = 0
         if acc_1>acc_2:
