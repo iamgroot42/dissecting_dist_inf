bad8afd3834a0447b2ced65d5dd97d5090861bcd
Anshuman Suri <iamgroot42@users.noreply.github.com>
2022-08-29 17:50:37 -0400

    Update label_KL.py

--- rebase/distribution_inference/attacks/blackbox/label_KL.py
+++ rebase/distribution_inference/attacks/blackbox/label_KL.py
@@ -42,16 +42,16 @@ class label_only_KLAttack(Attack):
         thre = 0.5 if not_using_logits else 0
         # Get values using data from first distribution
         preds_1_first, preds_1_second = self._get_kl_preds(
-            preds_adv.preds_on_distr_1.preds_property_1>=thre,
-            preds_adv.preds_on_distr_1.preds_property_2>=thre,
-            preds_vic.preds_on_distr_1.preds_property_1>=thre,
-            preds_vic.preds_on_distr_1.preds_property_2>=thre)
+            1.*(preds_adv.preds_on_distr_1.preds_property_1>=thre),
+            1.*(preds_adv.preds_on_distr_1.preds_property_2>=thre),
+            1.*(preds_vic.preds_on_distr_1.preds_property_1>=thre),
+            1.*(preds_vic.preds_on_distr_1.preds_property_2>=thre))
         # Get values using data from second distribution
         preds_2_first, preds_2_second = self._get_kl_preds(
-            preds_adv.preds_on_distr_2.preds_property_1>=thre,
-            preds_adv.preds_on_distr_2.preds_property_2>=thre,
-            preds_vic.preds_on_distr_2.preds_property_1>=thre,
-            preds_vic.preds_on_distr_2.preds_property_2>=thre)
+            1.*(preds_adv.preds_on_distr_2.preds_property_1>=thre),
+            1.*(preds_adv.preds_on_distr_2.preds_property_2>=thre),
+            1.*(preds_vic.preds_on_distr_2.preds_property_1>=thre),
+            1.*(preds_vic.preds_on_distr_2.preds_property_2>=thre))
 
         # Combine data
         preds_first = np.concatenate((preds_1_first, preds_2_first), 1)
@@ -70,9 +70,17 @@ class label_only_KLAttack(Attack):
         choice_information = (None, None)
         return [(acc, preds), (None, None), choice_information]
     def _get_kl_preds(self, ka, kb, kc1, kc2):
-        # Apply sigmoid to ones that are not already sigmoided
-        ka_, kb_ = np.expand_dims(np.mean(ka,axis=1),axis=1),np.expand_dims(np.mean(kb,axis=1),axis=1)
-        kc1_, kc2_ = np.expand_dims(np.mean(kc1,axis=1),axis=1), np.expand_dims(np.mean(kc2,axis=1),axis=1)
+        # Convert 0/1 predictions to (0+eps, 1-eps)
+        small_eps = 1e-2
+        ka_, kb_, kc1_, kc2_ = ka, kb, kc1, kc2
+        ka_[ka_ == 0] = small_eps
+        kb_[kb_ == 0] = small_eps
+        kc1_[kc1_ == 0] = small_eps
+        kc2_[kc2_ == 0] = small_eps
+        ka_[ka_ == 1] = 1 - small_eps
+        kb_[kb_ == 1] = 1 - small_eps
+        kc1_[kc1_ == 1] = 1 - small_eps
+        kc2_[kc2_ == 1] = 1 - small_eps
         
         # Consider all unique pairs of models
         xx, yy = np.triu_indices(ka.shape[0], k=1)
