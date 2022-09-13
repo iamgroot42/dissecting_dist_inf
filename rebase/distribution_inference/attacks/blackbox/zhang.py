082073d4431ed4d70baa7b3e16a676d416537968
Anshuman Suri <as9rw@virginia.edu>
2022-09-13 15:40:49 -0400

    Implement Zhang's BB attack

--- rebase/distribution_inference/attacks/blackbox/zhang.py
+++ rebase/distribution_inference/attacks/blackbox/zhang.py
@@ -35,6 +35,34 @@ class ZhangAttack(Attack):
                 preds.append(result[0][1])
             return [(accs, preds), (None, None), (None,None)]
 
+    def _attack_on_data(self,
+                        preds_adv: PredictionsOnOneDistribution,
+                        preds_vic: PredictionsOnOneDistribution,
+                        not_using_logits: bool = False):
+        ka, kb = preds_adv.preds_property_1, preds_adv.preds_property_2
+        kc1, kc2 = preds_vic.preds_property_1, preds_vic.preds_property_2
+        # Apply sigmoid to ones that are not already sigmoided
+        if not self.not_using_logits:
+            ka, kb = sigmoid(ka), sigmoid(kb)
+            kc1, kc2 = sigmoid(kc1), sigmoid(kc2)
+        
+        # Use same architecture as in paper
+        self.meta_model = MLPClassifier((20, 8), early_stopping=True)
+
+        # Train BB meta-classifier
+        X_train = np.concatenate((ka, kb), axis=0)
+        y_train = np.concatenate((np.ones(len(ka)), np.zeros(len(kb))), axis=0)
+        self.meta_model.fit(X_train, y_train)
+        val_acc = self.meta_model.best_validation_score_
+
+        # Predict on victim
+        X_test = np.concatenate((kc1, kc2), axis=0)
+        y_test = np.concatenate((np.ones(len(kc1)), np.zeros(len(kc2))), axis=0)
+        y_pred = self.meta_model.predict_proba(X_test)[:, 1]
+        test_acc = self.meta_model.score(X_test, y_test)
+
+        return y_pred, (val_acc, test_acc)
+
     def attack_not_epoch(self,
                preds_adv: PredictionsOnDistributions,
                preds_vic: PredictionsOnDistributions,
@@ -45,12 +73,18 @@ class ZhangAttack(Attack):
         self.not_using_logits = not_using_logits
         self.meta_model = MLPClassifier((20, 8)) # Use same architecture as in paper
 
-        # TODO: Implement
+        # Try attack with data from both distributions
+        preds_1, (tr_acc_1, te_acc_1) = self._attack_on_data(preds_adv.preds_on_distr_1, preds_vic.preds_on_distr_1)
+        preds_2, (tr_acc_2, te_acc_2) = self._attack_on_data(preds_adv.preds_on_distr_2, preds_vic.preds_on_distr_2)
 
+        # Pick the one that performs best locally
+        if tr_acc_1 > tr_acc_2:
+            victim_pred_use, victim_acc_use = preds_1, te_acc_1
+        else:
+            victim_pred_use, victim_acc_use = preds_2, te_acc_2
 
         # No concept of "choice" (are we in the Matrix :P)
-        # choice_information = (None, None)
-        # return [(acc, preds), (None, None), choice_information]
+        return [(victim_acc_use, victim_pred_use), (None, None), (None,None)]
 
 
 def sigmoid(x):
